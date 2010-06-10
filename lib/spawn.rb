7651898237a060c458d3d467bb142a65695b523b
Will Bryant <will.bryant@gmail.com>
2010-06-11 01:36:49 +1200

    If Rails.cache is a memcache store, close the memcache server connection(s) after forking, so the parent's won't get shared

--- lib/spawn.rb
+++ lib/spawn.rb
@@ -99,8 +99,10 @@ module Spawn
 
         # disconnect from the listening socket, et al
         Spawn.close_resources
-        # get a new connection so the parent can keep the original one
+        # get a new database connection so the parent can keep the original one
         ActiveRecord::Base.spawn_reconnect
+        # close the memcache connection so the parent can keep the original one
+        Rails.cache.reset if Rails.cache.respond_to?(:reset)
 
         # run the block of code that takes so long
         yield
