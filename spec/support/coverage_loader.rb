0086b424826c309041aa95d6d39a5a9bc9ce4479
Michael Noack <michael@noack.com.au>
2016-10-20 09:14:06 +1030

    Display current coverage

--- spec/support/coverage_loader.rb
+++ spec/support/coverage_loader.rb
@@ -19,8 +19,9 @@ unless ENV['COVERAGE'] == 'off'
   SimpleCov.at_exit do
     SimpleCov.result.format!
     percent = SimpleCov.result.covered_percent
+    puts "Coverage is #{"%.2f" % percent}%"
     unless percent >= MINIMUM_COVERAGE
-      puts "Coverage must be above #{MINIMUM_COVERAGE}%. It is #{"%.2f" % percent}%"
+      puts "Coverage must be above #{MINIMUM_COVERAGE}%"
       Kernel.exit(1)
     end
   end
