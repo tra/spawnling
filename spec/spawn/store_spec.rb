395a3bb21839a3ccdd1d51afa5478a5789b43da1
Andrew Clemons <andrew.clemons@gmail.com>
2018-10-08 09:14:30 +1300

    Silence rspec deprecation warnings
    Run spawn_spec and store_spec through transpec to silence deprecation
    warnings when running specs.

--- spec/spawn/store_spec.rb
+++ spec/spawn/store_spec.rb
@@ -3,10 +3,10 @@ require 'spec_helper'
 describe Store do
   it 'should flag/unflag' do
     Store.reset!
-    Store.flag.should be_falsey
+    expect(Store.flag).to be_falsey
     Store.flag!
-    Store.flag.should be_truthy
+    expect(Store.flag).to be_truthy
     Store.reset!
-    Store.flag.should be_falsey
+    expect(Store.flag).to be_falsey
   end
 end
